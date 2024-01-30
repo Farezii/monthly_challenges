c0e9178dfe08ed941155fb5d3c558c935a91985e
Farezii <fpcrezende@gmail.com>
2024-01-30 14:58:03 -0400

    Created first views, first urls

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def january(request):
+    return HttpResponse('Try to sleepa t least 8 hours a day')
+
+def february(request):
+    return HttpResponse('Walk 20 minutes everyday')
